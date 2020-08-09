ea9ea9a93d845517779977576986c84dee2cdb2f
a <alexeybulmeni@gmail.com>
2020-08-08 21:20:50 -0700

    Added borders around dice, and a prnt_dice() function

--- dice.py
+++ dice.py
@@ -1,5 +1,5 @@
-d1, d2 = "⚬     ⚬", "       "
-d3, d4 = "   ⚬   ", "      ⚬"
+d1, d2 = "|⚬     ⚬|", "|       |"
+d3, d4 = "|   ⚬   |", "|      ⚬|"
 dice_list = [
     f"{d2}\n{d3}\n{d2}",
     f"{d4[::-1]}\n{d2}\n{d4}",
@@ -7,4 +7,7 @@ dice_list = [
     f"{d1}\n{d2}\n{d1}",
     f"{d1}\n{d3}\n{d1}",
     f"{d1} \n{d1} \n{d1}"]
-
+def prnt_dice(num):
+    print(" "+"_"*7)
+    print(dice_list[int(num)-1])
+    print(" "+"‾"*7)
