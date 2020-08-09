ec9bf1fdd5bc2f83ec8063fc638e433b474ca2b6
a <alexeybulmeni@gmail.com>
2020-08-08 21:42:42 -0700

    Added color, imported only sleep() in time module

--- hangman.py
+++ hangman.py
@@ -1,13 +1,25 @@
-import time
+from time import sleep
 import requests
 from random import choice
+
+
+class colors:
+    PURPL = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[32m'
+    YELLO = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 response = requests.get("http://raw.githubusercontent.com/sindresorhus/mnemonic-words/master/words.json")
 data = response.json()
 
 name = input("What is your name, good sir?\n")
-print(f"Welcome to Hangman, {name}! Your game well begin shortly, so do not fret!")
-time.sleep(1)
+print(f"{colors.PURPL}Welcome to Hangman, {name}! Your game will begin shortly, so do not fret!{colors.ENDC}")
+sleep(1)
 counter = 0
 while True:
     word = choice(data)
@@ -16,7 +28,7 @@ while True:
     guessed = ""
     guessed_true = False
     if counter > 0:
-        playAgain = input(f"{name}, would you like to play again?\n")
+        playAgain = input(f"{colors.BLUE}{name}, would you like to play again?\n{colors.ENDC}")
         if playAgain.lower() == "no":
             break
     while turns > 0:
@@ -30,17 +42,17 @@ while True:
             print(f"Nice! You successfully finished!")
             guessed_true = True
             break
-        print(f"You have {turns} turns left!\n")
+        print(f"You have {colors.BLUE}{colors.BOLD}{turns}{colors.ENDC} turns left!\n")
         print(guessed)
         guess = input("What is your guess?\n").lower()
         if guess in word and len(guess) == 1:  # if the guess is in the word
             if guess in guesses:  # if the person guessed that already
-                print("You already guessed that, dummy!")
+                print(f"{colors.RED}You already guessed that, dummy!{colors.ENDC}")
                 turns -= 1
             else:
-                print("Congrats! That was a letter!")
+                print(f"{colors.GREEN}Congrats! That was a letter!{colors.ENDC}")
         else:
-            print("Wrong!")
+            print(f"{colors.RED}Wrong!{colors.ENDC}")
             turns -= 1
         guesses.append(guess)
     if not guessed_true:
