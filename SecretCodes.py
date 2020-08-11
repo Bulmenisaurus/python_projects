a54ec087b06f5d03edd26e9960fae3f9ebbd1373
a <alexeybulmeni@gmail.com>
2020-08-10 20:16:14 -0700

    Encoding done

--- SecretCodes.py
+++ SecretCodes.py
@@ -1,4 +1,16 @@
 # secret codes
-def encode(encode):
-    for i in encode:
-        encoded +=
+# chr() int -> str, ord() str --> int
+import random
+
+
+def encode(encode_input):
+    shift = random.randint(0, 15000)
+    encoded = chr(shift+1500)  # to make it seem more random
+    print(shift)
+    for i in encode_input:
+        encoded += chr((ord(str(i))+shift))
+    return encoded
+
+
+while True:
+    print(encode(input()))
