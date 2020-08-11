d6677b92bc219508c20c06783b77bc25dd1f48f2
a <alexeybulmeni@gmail.com>
2020-08-11 10:18:28 -0700

    removed \n at end of message

--- SecretCodes.py
+++ SecretCodes.py
@@ -22,22 +22,24 @@ def decode(decode_input):
             try:
                 decoded += chr((ord(str(i))-shift))
             except:
-                print(f"{i} is bugged. :(")
+                print(f"We're pretty sure something went wrong, ecpecially with '{i}'")
     return decoded
 
 
 print("Welcome to the Hackers Encrpyt Secret Messages machine, or HESM for short.")
-sleep(.5)
+sleep(.7)
 print("Simply type \"Quit\" to stop.")
-sleep(.5)
+sleep(.7)
 print("Good luck on your hacking adventures!")
-sleep(.5)
+sleep(.7)
 while True:
     choice = input("Would you like to decode or encode a message?\n").lower()
     if choice[0] == "e":
-        print(encode(input("What would you like to encode?\n")), "\n")
+        print(encode(input("What would you like to encode?\n")))
+        print()
     elif choice[0] == "d":
-        print(decode(input("What would you like to decode?\n")), "\n")
+        print(decode(input("What would you like to decode?\n")))
+        print()
     elif choice == "quit":
         break
     else:
