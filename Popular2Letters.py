5c889ebfefdc47616a07179d1a85b4ced782c525
a <alexeybulmeni@gmail.com>
2020-08-13 11:46:45 -0700

    Massive update (1/2) functions, optimizations, etc

--- Popular2Letters.py
+++ Popular2Letters.py
@@ -1,33 +1,77 @@
 import requests
 from PIL import Image, ImageDraw
 import string
-response = requests.get("http://raw.githubusercontent.com/sindresorhus/mnemonic-words/master/words.json")
-data = response.json()
-
-TwoLetters = {}
-for x in data:
-    for i in range(len(x)-1):
-        if x[i:i+2] in TwoLetters:
-            TwoLetters[x[i:i+2]] += 1
-        else:
-            TwoLetters[x[i:i + 2]] = 1
-SortedTwoLetters = {k: v for k, v in sorted(TwoLetters.items(), key=lambda item: item[1])}
-SortedTwoLetters = dict(zip([x for x in SortedTwoLetters.keys()][::-1], [x for x in SortedTwoLetters.values()][::-1]))
-for x in SortedTwoLetters:
-    if SortedTwoLetters[x] > 0:
-        print(f"{x}:  {SortedTwoLetters[x]}")
+# response = requests.get("http://raw.githubusercontent.com/sindresorhus/mnemonic-words/master/words.json")
+# data = response.json()
 
 
+def open_dict(choice_input):
+    if choice_input == "s":
+        response = requests.get("http://raw.githubusercontent.com/sindresorhus/mnemonic-words/master/words.json")
+        open_dict_return = response.json()
+    else:
+        open_dict_return = []
+        with open("Dictionary.txt", 'r') as english:
+            open_dict_return = english.readlines()
+            for x in range(len(open_dict_return)-1):  # last line is empty
+                open_dict_return[x] = open_dict_return[x].strip("\n")
+    return open_dict_return
+
+
+def find_letters(find_input):
+    find_letters_dict = {}
+    for x in find_input:
+        if len(x) >= 3:  # words have to be longer or equal to 3 letters
+            for i in range(len(x) - 1):  # goes through each combination
+                if x[i:i + 2] in find_letters_dict:
+                    find_letters_dict[x[i:i + 2]] += 1
+                else:
+                    find_letters_dict[x[i:i + 2]] = 1
+    return find_letters_dict
+
+
+def sort_letters(sort_input):
+    sorted_input = {k: v for k, v in sorted(sort_input.items(), key=lambda item: item[1])}
+    sorted_input = dict(zip([x for x in sorted_input.keys()][::-1], [x for x in sorted_input.values()][::-1]))
+    return sorted_input
+
+
+def top_letters(top_input, length_of_sample):
+    print("Here are the 10 most common word combos:")
+    keys_list, values_list = [], []
+    counter = 0
+    for key, value in top_input.items():
+        if counter == 10:
+            break
+        percentage = (value / length_of_sample) * 100
+        counter += 1
+        print(f"{key} occurred in {percentage}% of all the words.")
+
+
+choice = input("Which dictionary would you like to use? (small/big)\n").lower()[0]
+data = open_dict(choice[0].lower())
+sorted_data = sort_letters(find_letters(data))
+print(f"Length of our data so far is {len(sorted_data)}.")
+top_letters(sorted_data, len(data))
+quit()
 ###################################
 ###################################
 # graphs all the points on an image
 
-main_img = Image.new("LA", (26, 26), "#FFF")  # 8-bit black/white with alpha
+main_img = Image.new("LA", (28, 28), "#FFF")  # 8-bit black/white with alpha
 draw = ImageDraw.Draw(main_img)
+count = 0
 for x in SortedTwoLetters.keys():
-    x_pos = string.ascii_lowercase.index(x[0])
-    y_pos = string.ascii_lowercase.index(x[1])
-    draw.point((x_pos, y_pos), fill="#000")
-
-draw.point((0, 0), fill=(000, 100))
+    try:
+        x_pos = string.ascii_lowercase.index(x[0]) + 1
+        y_pos = string.ascii_lowercase.index(x[1]) + 1
+        opacity = SortedTwoLetters[x] * (255 / max(SortedTwoLetters.values()))  # more common combinations are darker
+        draw.point((x_pos, y_pos), fill=(000, round(opacity)))  # draw da point
+    except ValueError:
+        idgaf_about_this_bug = True
+    count += 1
+draw.line((0, 0, 0, 27), fill="#000")
+draw.line((0, 0, 27, 0), fill="#000")
+draw.line((27, 0, 27, 27), fill="#000")
+draw.line((27, 27, 0, 27), fill="#000")
 main_img.show()
