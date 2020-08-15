afb64cd2b65e25440a2d51fa7f206b916efd3878
a <alexeybulmeni@gmail.com>
2020-08-14 20:39:09 -0700

    🎊 Initial commmit

---
+++ RadixGIF.py
@@ -0,0 +1,10 @@
+def main():
+    buckets = []  # sets up bucket
+    for x in range(9):
+        buckets.append([])
+    print(buckets)
+    length = input("How long would you like your list to be?")
+
+
+if __name__ == "__main__":
+    main()
