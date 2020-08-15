f793126181b119e838c0659729b21126ace6f01a
a <alexeybulmeni@gmail.com>
2020-08-14 23:16:58 -0700

    ➕ finished radix sort. Implementing gif support tommorow.

--- Project o' day/RadixGIF.py
+++ Project o' day/RadixGIF.py
@@ -1,8 +1,12 @@
 import random
+from functools import reduce  # reduces the darn fudgin lists (for loops are FOR losers)
 
 
 def radix_sort(radix_input):
     radix_sorted = buckets(0, radix_input)
+    for x in range(len(str(max(radix_input)))-1):
+        radix_sorted = buckets(x+1, radix_sorted)
+        print(radix_sorted)
 
 
 def buckets(digit, bucket_radix):
@@ -11,12 +15,11 @@ def buckets(digit, bucket_radix):
         buckets_list.append([])  # sets up the buckets
     for x in bucket_radix:
         x = str(x)
-        if len(x) < digit:
-            buckets_list[0].append(0)  # digits[x] is outside of x, input 0 by default
+        if len(x) <= digit:
+            buckets_list[0].append(int(x))  # digits[x] is outside of x, input 0 by default
         else:
            buckets_list[int(x[::-1][digit])].append(int(x))
-    print(buckets_list)
-    return buckets_list
+    return reduce(lambda x, y: x+y, buckets_list)
 
 
 length = int(input("How many integers long would you like your list to be?\n"))  # length of list
