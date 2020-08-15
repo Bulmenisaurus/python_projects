e153d04c8a340b8872af98db4396baf68ed9964a
a <alexeybulmeni@gmail.com>
2020-08-14 18:09:06 -0700

    ➕🕷🔨 Masssive overhaul and bug fixes

--- Project o' day/Visualize bytes in files.py
+++ Project o' day/Visualize bytes in files.py
@@ -1,10 +1,11 @@
 import os
 from collections import Counter
 from PIL import Image, ImageDraw
+from math import sqrt
 
-file = input("What is the path to your file?\n")
+file = "Dictionary.txt"
 head, tail = os.path.split(file)
-print(f"Opening file\033[1m{tail}"+'\033[0m')
+print(f"Opening file \033[1m{tail}"+'\033[0m')
 
 byte_size = os.path.getsize(file)  # tells me the size of the file in bytes
 with open(file, "rb") as fi:       # reads file as bytes
@@ -16,30 +17,30 @@ print(len(contents))
 ################################
 # Graphs all the points!
 
-
-def lerp(a, b, percent):
+pair_counts = {}
+def lerp(a, b, percent):  # x percent of the way between to numbers. Very useful!
     return a + (b - a) * (percent/100)
 
 
-occurrences = dict(Counter(contents))
-popular_byte_count = next(iter(occurrences.values()))
-
 count = 0
-main_img = Image.new("RGB", (256, 256), "#FFF")
-draw = ImageDraw.Draw(main_img)
-rangenum = len(occurrences.values())-2
-for x in range(len(occurrences.values())-2):
 
-    x_pos = list(occurrences.keys())[x]
-    y_pos = list(occurrences.keys())[x+1]
+dimensions = input("Would you like your graph to be 1d or 2d?\n")
 
-    value = ((occurrences[x_pos] + occurrences[y_pos])/2)/popular_byte_count * 255
-
-    if value > 255:
-        value = 255
-    color = (round(lerp(0, 255, value)), 0, round(lerp(0, 255, value)))
-    draw.point((x_pos, y_pos), color)  # draw da point
-    count += 1
+if dimensions[0] == "2":
+    main_img = Image.new("RGB", (256, 256), "#FFF")
+    draw = ImageDraw.Draw(main_img)
+    rangenum = len(contents)-1
+    for x in range(len(contents)-1):
+        x_pos = contents[x]  # x position of the dot is x'th number
+        y_pos = contents[x+1]
+        if (x_pos, y_pos) in pair_counts:
+            pair_counts[(x_pos, y_pos)] += 1  # if (x, y) already in dict, change their count by 1
+        else:
+            pair_counts[(x_pos, y_pos)] = 1  # otherwise, add them to the dict with a count of 1
+    for j in pair_counts:
+        value = round(min(pair_counts[j], 20000) / 20000 * 255)
+        color = (value, 30, value)
+        draw.point((j[0], j[1]), color)
 
 print(count)
 main_img.show()
