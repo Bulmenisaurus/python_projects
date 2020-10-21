3ae0498c990010c7960233eed9c6fbbedfe74faf
a <alexeybulmeni@gmail.com>
2020-10-21 12:08:07 -0700

    Added word creatin support

--- Project o' day/new_word_generator_#2.py
+++ Project o' day/new_word_generator_#2.py
@@ -1,20 +1,61 @@
 """
-Oopen file, extrcat
+Open file, extract data
+Add letters of each word, append to dictinoary
+build new words off of that
 """
+import string
+from random import shuffle, choice
+
+
+def extract_wordpatterns(wordpattern_data):
+    valid_letters = string.ascii_letters
+
+    wordpatterns = {i: "" for i in " "+string.ascii_lowercase}  # initialize empty alphabet {a="", b="", etc}
+    for wrd in wordpattern_data:
+        for index, lttr in enumerate(wrd):
+            try:  # detect if lttr is last
+                wrd[index+1]
+            except IndexError:
+                # This code is run when lttr is the last letter in a word
+                wordpatterns[lttr] = ' '  # space (should) mean End of word
+            else:
+                if lttr in valid_letters:
+                    # This code is run when lttr isn't
+                    wordpatterns[lttr] += wrd[index+1]
+                if index == 0:
+                    wordpatterns[' '] += lttr  # beginning of word
+
+    wordpatterns = {i: ''.join(sorted(wordpatterns[i])) for i in wordpatterns}
+    return wordpatterns
+
+
+def gen_words_from_patterns(word_patterns, amount=1, min_len=2, capitalize=False):
+    underline = '̱'
+    words = []
+    while not len(words) >= amount:
+        word = choice(word_patterns[' '])
+        while True:
+            new_letter = choice(word_patterns[word[-1]])
+            word += new_letter
+            if word[-1] == ' ':
+                break
+        word = word.replace(' ', '')
+        if len(word) > min_len:
+            words.append(word.capitalize() if capitalize else word)
+    return words
 
 
 with open("../Dictionary.txt", "r") as dict_:
-    data_ = dict_.read().splitlines()[:1000]
+    data_ = dict_.read().splitlines()
+    shuffle(data_)
+    data_ = data_[:100]
 
-valid_letters = list("abcdefghijklmnopqrstuvwxyz")
-valid_letters += [i.upper() for i in valid_letters]
-print(valid_letters)
+word_data = extract_wordpatterns(data_)
+# print(word_data)
+generated_words = gen_words_from_patterns(word_data, amount=20, min_len=3, capitalize=True)
+print(sorted(generated_words))
 
 
-print(data_)
-
-for wrd in data_:
-    for index, lttr in enumerate(wrd)
 
 
 
