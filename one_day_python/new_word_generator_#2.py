46651add1f776d87572ebb0afa627b9591009171
a <alexeybulmeni@gmail.com>
2020-10-22 18:43:21 -0700

    Trying to add space support

--- one_day_python/new_word_generator_#2.py
+++ one_day_python/new_word_generator_#2.py
@@ -10,20 +10,21 @@ from random import shuffle, choice
 def extract_wordpatterns(wordpattern_data):
     valid_letters = string.ascii_letters
 
-    wordpatterns = {i: "" for i in " "+string.ascii_lowercase}  # initialize empty alphabet {a="", b="", etc}
+    wordpatterns = {i: "" for i in "| "+string.ascii_lowercase}  # initialize empty alphabet {a="", b="", etc}
     for wrd in wordpattern_data:
+        wrd = wrd.lower()
         for index, lttr in enumerate(wrd):
             try:  # detect if lttr is last
                 wrd[index+1]
             except IndexError:
                 # This code is run when lttr is the last letter in a word
-                wordpatterns[lttr] = ' '  # space (should) mean End of word
+                wordpatterns[lttr] = '|'  # (should) mean End of word
             else:
                 if lttr in valid_letters:
                     # This code is run when lttr isn't
                     wordpatterns[lttr] += wrd[index+1]
                 if index == 0:
-                    wordpatterns[' '] += lttr  # beginning of word
+                    wordpatterns['|'] += lttr  # beginning of word
 
     wordpatterns = {i: ''.join(sorted(wordpatterns[i])) for i in wordpatterns}
     return wordpatterns
@@ -33,13 +34,13 @@ def gen_words_from_patterns(word_patterns, amount=1, min_len=2, capitalize=False
     underline = '̱'
     words = []
     while not len(words) >= amount:
-        word = choice(word_patterns[' '])
+        word = choice(word_patterns['|'])
         while True:
+            print(f"{word_patterns[word[-1]]=}, {word}")
             new_letter = choice(word_patterns[word[-1]])
             word += new_letter
-            if word[-1] == ' ':
+            if word[-1] == '|':
                 break
-        word = word.replace(' ', '')
         if len(word) > min_len:
             words.append(word.capitalize() if capitalize else word)
     return words
@@ -47,13 +48,12 @@ def gen_words_from_patterns(word_patterns, amount=1, min_len=2, capitalize=False
 
 with open("../python_data/Dictionary.txt", "r") as dict_:
     data_ = dict_.read().splitlines()
-    shuffle(data_)
-    data_ = data_[:100]
 
 word_data = extract_wordpatterns(data_)
-# print(word_data)
-generated_words = gen_words_from_patterns(word_data, amount=20, min_len=3, capitalize=True)
-print(sorted(generated_words))
+print(word_data)
+generated_words = gen_words_from_patterns(word_data, amount=300, min_len=3, capitalize=True)
+print(a := [x.replace('|', '').lower() for x in sorted(generated_words)])
+
 
 
 
