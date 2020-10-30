1fcb57930d5edde9061e7c19d52dfcfb0881e513
a <alexeybulmeni@gmail.com>
2020-10-30 10:26:40 -0700

    If function returns none, dont use it

--- one_day_python/obfuscate.py
+++ one_day_python/obfuscate.py
@@ -19,14 +19,17 @@ def obfuscate_num(num: int):
 
     def set_obfuscate(num_: int):
         """Using x unique characters and sets"""
-        unique_characters=list(string.printable)if getrandbits(1)or num>10 else list('{[)_,.><\"\'')
+        if num_ < 0:
+            return
+
+        unique_characters = list(string.printable)if getrandbits(1) or num > 10 else list('{[)_,.><\"\'')
         shuffle(unique_characters)
-        unique_characters = list(''.join([x * randint(1, 7) for x in unique_characters[:num_]]))
+        unique_characters = list(''.join([k * randint(1, 7) for k in unique_characters[:num_]]))
         shuffle(unique_characters)
         if getrandbits(1):
             return "len(set("+repr(''.join(unique_characters))+"))"
         else:
-            return "len("+repr(unique_characters).replace('[', '{').replace(']', '}')+")"
+            return repr(unique_characters).replace('[', '{').replace(']', '}')+"__len__()"
 
     def bit_obfuscate(num_: int):
         """Uses << and >> operators to make seemingly improper equations (to non programmers)"""
@@ -35,6 +38,9 @@ def obfuscate_num(num: int):
 
     def boolsum_obfuscate(num_: int):
         """Worst ducking thing here, sum of a number of booleans"""
+
+        if num_ < 0:
+            return
         trues, falses = (num_ if num_> 0 else 0, -num_ if num_ < 0 else 0)
         truffset = randint(*sorted((num_//2, -num_//3)))  # adds random number of trues or falses
         trues += truffset
@@ -45,12 +51,16 @@ def obfuscate_num(num: int):
         return "sum("+repr(bool_list)+")"
 
 
-    return choice([
-        uni_obfuscate(num),
-        set_obfuscate(num),
-        bit_obfuscate(num),
-        boolsum_obfuscate(num)
-    ])
+    choices = []
+    funcs = [uni_obfuscate, set_obfuscate,
+             bit_obfuscate, boolsum_obfuscate,
+             ]
+    for x in funcs:
+        if (obfuscated_choice := x(num)) is not None:
+            choices.append(obfuscated_choice)
+
+    return choice(choices)
 
 
-print(obfuscate_num(-69))
+
+print(obfuscate_num(32000))
