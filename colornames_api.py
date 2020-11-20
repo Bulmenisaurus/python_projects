8b33dae2e2e46ea73f363f93ad3af05c75f104c7
a <alexeybulmeni@gmail.com>
2020-11-19 16:37:43 -0800

    Pycharm - commit test

---
+++ colornames_api.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+class Colors:
+    PURPL = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[32m'
+    YELLO = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
