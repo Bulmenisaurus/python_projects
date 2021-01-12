90d4031d954f55f2160a6fc3bd9b6348d68d41f3
a <alexeybulmeni@gmail.com>
2021-01-12 10:26:26 -0800

    New evil intent file

---
+++ evil_intent/autoclicker.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.001
+
+while True:
+    input('Press ENTER to start ')
+    time.sleep(2)
+    timeStart = time.time()
+    while True:
+        pyautogui.click()
+
+        if (time.time() - timeStart) > 600:
+            break
