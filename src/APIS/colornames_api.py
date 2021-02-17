07ca4d6489de9aeb9a1a01f53a48042ec64a3d0e
a <alexeybulmeni@gmail.com>
2021-02-16 16:52:58 -0800

    Apparently terminal can display colors!

--- src/APIS/colornames_api.py
+++ src/APIS/colornames_api.py
@@ -12,21 +12,11 @@ if sys.argv[0] != 'python':
         ENDC = '\033[0m'
         BOLD = '\033[1m'
         UNDERLINE = '\033[4m'
-else:
-    class Colors:
-        PURPL = ''
-        BLUE = ''
-        GREEN = ''
-        YELLO = ''
-        RED = ''
-        ENDC = ''
-        BOLD = ''
-        UNDERLINE = ''
 
 if sys.argv[0] != 'python':
     def get_color_escape(r, g, b, background=False):
         # from https://stackoverflow.com/a/45782972
-        return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b) + '\033[0m'
+        return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
 else:
     def get_color_escape(*args):
         return ''
@@ -48,13 +38,11 @@ def fresh_colors() -> list:
 
 def format_clr(clr: dict) -> str:
     rgb = tuple(int(clr['hexCode'][i:i + 2], 16) for i in (0, 2, 4))
-    color = get_color_escape(*rgb) + clr['hexCode']
+    color = get_color_escape(*rgb) + clr['hexCode'] + '\033[0m'
 
-    formatstring = (clr['name'],  # name of color, such as `red`
-                    color,  # hex code of color, such as `#ff0000`
-                    ("ID: " if 'nameId' in clr else '') + str(clr.get('nameId')))  # id of color, such as `ID: 1`
+    formatstring = (clr['name'], color)
 
-    formatted_clr = "{}: #{}\n{}\n".format(*formatstring)
+    formatted_clr = "{}: #{}\n\n".format(*formatstring)
     return formatted_clr
 
 
@@ -62,7 +50,7 @@ print(f"""\nWould you like to:
  {Colors.PURPL}[0]{Colors.ENDC} 100 FRESHEST colors
  {Colors.BLUE}[1]{Colors.ENDC} See the name of a specific color (1/16 chance it exists)
  {Colors.GREEN}[2]{Colors.ENDC} raw data of 100 freshest colors
- {Colors.YELLO}[3]{Colors.ENDC} raw data of specific color""")
+ {Colors.YELLO}[3]{Colors.ENDC} raw data of specific color\n""")
 
 choice = input()
 
@@ -71,11 +59,12 @@ if choice == '0':
     print(''.join([format_clr(x) for x in fresh_colors()]))
 elif choice == '1':
     # see spe
-    hex_clr = clr_from_hex(input("What hexadecimal color would you like to view?"))
+    hex_clr = clr_from_hex(input("What hexadecimal color would you like to view?\n"))
     print(format_clr(hex_clr))
 elif choice == '2':
-    ''
+    print(fresh_colors())
 elif choice == '3':
-    ''
+    hex_clr = clr_from_hex(input("What hexadecimal color would you like to view?\n"))
+    print(hex_clr)
 else:
     print("Not a valid option :(")
