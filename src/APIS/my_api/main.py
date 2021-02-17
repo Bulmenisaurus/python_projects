94d4fdac9be36f64464921a98ee0fa0ba4a62b3d
a <alexeybulmeni@gmail.com>
2021-02-16 20:22:51 -0800

    Attempted to filter data by kwargs;

--- src/APIS/my_api/main.py
+++ src/APIS/my_api/main.py
@@ -1,10 +1,24 @@
 import urllib.parse
+from typing import *
 
 url = 'https://a.com/asd?length=2&isAscii=false'
 parsed_url = urllib.parse.urlparse(url)
 parsed_url_query = urllib.parse.parse_qs(parsed_url.query)
+#parsed_url_query = {x: x[0]}
 
-print(f"{parsed_url_query=}")
+QueryArgs = List[Tuple[str, List]]
 
-def response_to_query(query: dict) -> dict:
-    pass
+
+def response_to_query(query:  Dict[str, List[str]]) -> dict:
+    query_args: QueryArgs = list(query.items())
+
+    a = filter_using_query([{"length": "2", "isAscii": "false", "emoji": "Ω"}], **query)
+    print(list(a))
+
+
+def filter_using_query(data, **query):
+    for emoticon in data:
+        if emoticon.items() <= query.items():
+            yield emoticon
+
+response_to_query(parsed_url_query)
