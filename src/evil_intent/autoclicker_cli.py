88b89b878c32483ae591002aad950afdf119b353
a <alexeybulmeni@gmail.com>
2021-02-23 21:07:06 -0800

    CLI autoclicker for more convenience

---
+++ src/evil_intent/autoclicker_cli.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import pyautogui
+
+"""
+can be run with:
+> python3 autoclicker_cli.py 5
+
+to left click for 5 secs
+"""
+
+duration = sys.argv[1]
+mouse_button = sys.argv[2] if len(sys.argv) >= 3 else 'left'
+
+pyautogui.PAUSE = 0.001
+
+print(f"Clicking for {duration} seconds with the {mouse_button} click button")
+time.sleep(2)
+
+start_time = time.time()
+clicks = 0
+while True:
+    clicks += 50
+    pyautogui.click(button=mouse_button, _pause=False, clicks=10)
+
+    if (time.time() - start_time) > int(duration):
+        break
+
+print(f"Clicked {clicks} times with the {mouse_button} click button")
